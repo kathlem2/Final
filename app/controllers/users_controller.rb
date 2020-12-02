f5d332791956691bd5f8311d85eac1acbc724503
DaGhost-git <nateyun8@gmail.com>
2020-12-02 14:39:43 -0500

    Added full login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        log_in @user
         format.html { redirect_to "#", notice: 'User successfully created!'}
       else
         format.html { render :new }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :email, format: { with: /[[:alnum:]+_.-]+@[[:alnum:].-]/ }, presence: true
   validates_uniqueness_of :email
   validates :name, presence: true
+
+  has_secure_password
   validates :password, format: { with: /[[:alnum:]!@#$%^&*()]{8,26}/ }, presence: true
 end
