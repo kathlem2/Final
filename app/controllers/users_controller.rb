9700a23c7305642a15ad67bac1dfab7e8885493d
kylehustek <kylehustek@gmail.com>
2020-12-02 21:46:02 -0500

    Working groupid with edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :isTeacher)
+      params.require(:user).permit(:name, :email, :password, :isTeacher, :group_id)
     end
 end
