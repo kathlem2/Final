4980cc066df68bd043ce34482b3a86947b32ca31
paige-bormann <bormann.12@osu.edu>
2020-12-04 13:26:53 -0500

    Shows users associated with groups and feedback associated with presentations.

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -10,6 +10,7 @@ class PresentationsController < ApplicationController
   # GET /presentations/1
   # GET /presentations/1.json
   def show
+    @feedbacks = @presentation.feedbacks
   end
 
   # GET /presentations/new
