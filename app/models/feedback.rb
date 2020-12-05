ee86651e0a96a674caa83d3f599272186f825ad3
paige-bormann <bormann.12@osu.edu>
2020-12-04 19:18:34 -0500

    Updated formatting and validations.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,6 @@
 class Feedback < ApplicationRecord
   belongs_to :presentation
+  validates :presentation_id, presence: true
+  validates :ranking, presence: true, :inclusion => 1..10
+  validates :comment, presence: true
 end
