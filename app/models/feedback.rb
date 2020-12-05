fa489afcfc863e48ea0855464e601c9d49ebaee8
kylehustek <kylehustek@gmail.com>
2020-12-04 20:21:18 -0500

    Fixed miscellaneous formatting, and added some comments

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,5 +4,7 @@ class Feedback < ApplicationRecord
   validates :presentation_id, presence: true
   validates :ranking, presence: true, :inclusion => 1..10
   validates :comment, presence: true
+  # Ensures that the pair of user id and presentation id for a given feedback are unique
+  # this makes it so that each user can only submit one feedback form for a given presentation.
   validates_uniqueness_of :user_id, :scope => [:presentation_id] 
 end
